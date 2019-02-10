f92559443b2a58850ce9954bbea35c96989d9691
suryamanchukonda <manchusurya@gmail.com>
2019-02-10 21:22:26 +0530

    solution for integer reversal is ready now

--- reverseint/index.js
+++ reverseint/index.js
@@ -1,4 +1,3 @@
-// --- Directions
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
 // --- Examples
@@ -8,6 +7,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    var revInt = n;
+    //If we do not store the apllied methods back to revInt variable then parseInt in the return statement will just take the argument of the function
+    revInt = revInt.toString().split('').reverse().join('')
+    //If the arguments passed are negative numbers than the reverse integer will become positive while parsing the string back to integer. Here Math.sign() is used to keep the negative number as it is.
+    return parseInt(revInt)*Math.sign(n);
+};
+
+//Checking if the code works..
+console.log(reverseInt(-53));
 
 module.exports = reverseInt;
+
+
