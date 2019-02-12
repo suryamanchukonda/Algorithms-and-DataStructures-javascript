0a74652cbee36b552fc77a5208e256bb32f00270
suryamanchukonda <manchusurya@gmail.com>
2019-02-12 20:09:51 +0530

    solution is now ready

--- maxchar/index.js
+++ maxchar/index.js
@@ -1,10 +1,32 @@
-// --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+
+function maxChar(str) {
+    var max = 0;
+    var charMax = '';
+    var charMap = {};
+    for(let char of str){
+        if(charMap[char]){
+            charMap[char]++;
+        }
+        else{
+            charMap[char] = 1;
+        }
+    }
+    for(let char in charMap){
+        if(charMap[char]>max && char !== ' '){
+            max = charMap[char]
+            charMax = char;
+        }
+    }
+    return charMax;
+}
+
+console.log(maxChar("Hey, how are you doing?"));
 
 module.exports = maxChar;
