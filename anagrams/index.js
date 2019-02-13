0689293f04aa069cd2eee7f104861d64359da554
suryamanchukonda <manchusurya@gmail.com>
2019-02-13 23:13:58 +0530

    solution is now ready

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false;
+    }
+
+    for(let char in aCharMap){
+        if(aCharMap[char] !== bCharMap[char]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function buildCharMap(str){
+    var charMap = {};
+
+    for(let char of str.replace(/[^\w]/g,"").toLowerCase()){
+        charMap[char] = charMap[char]+1 || 1;
+    }
+    return charMap;
+}
+
+console.log(anagrams("hey, what sup?", "Yo! my name is surya"));
 
 module.exports = anagrams;
